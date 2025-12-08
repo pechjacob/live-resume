1c92f9b84fa7ab420d5f94842c0b12da1f684a5d
Jacob Pech <pechjacob@gmail.com>
2025-12-08 13:22:20 -0700

    Disable BrowserSync plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import browserSyncPlugin from 'vite-plugin-browser-sync';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -13,7 +12,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       open: false,
     },
-    plugins: [react(), browserSyncPlugin()],
+    plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
