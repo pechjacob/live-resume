b994b9ad701091a644fddde9ba5d97b0525d7296
Jacob Pech <pechjacob@gmail.com>
2025-12-08 22:10:05 -0700

    Fix print contrast: Gray-300 background, Gray-400 separator
    - App.tsx: Experience background set to gray-300 (darker) to differentiate from sidebar.
    - index.css: Separator line darkened to gray-400 (#9ca3af) to ensure it is visible against the gray-300 background.

--- App.tsx
+++ App.tsx
@@ -208,7 +208,7 @@ function App() {
 
               {/* Right Column: Experience */}
               {/* Darker background in print (gray-300) as requested */}
-              <div className="hidden md:block w-full md:w-2/3 lg:w-[70%] bg-gray-50/50 md:bg-gray-50/90 dark:bg-transparent print:w-[70%] print:float-right print:bg-gray-200 print:text-black print:block order-2" id="experience">
+              <div className="hidden md:block w-full md:w-2/3 lg:w-[70%] bg-gray-50/50 md:bg-gray-50/90 dark:bg-transparent print:w-[70%] print:float-right print:bg-gray-300 print:text-black print:block order-2" id="experience">
                 <ExperienceSection />
               </div>
             </div>
